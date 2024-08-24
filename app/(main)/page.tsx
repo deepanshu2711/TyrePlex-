2cdfcc50b63247a4833b8daa16c63c95925a308b
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-24 18:09:30 +0530

    added main slider

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@mui/material";
-import PersonIcon from "@mui/icons-material/Person";
 import Header from "@/components/Header";
+import MainSlider from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-[#F5F6F9] min-h-screen">
       <Header />
+      <MainSlider />
     </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import {
+  A11y,
+  Autoplay,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const MainSlider = () => {
+  const itemData = [
+    "/image1.jpg",
+    "/image2.jpg",
+    "/image1.jpg",
+    "/image1.jpg",
+    "/image1.jpg",
+  ];
+  return (
+    <div className="">
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        navigation
+        spaceBetween={10}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{ delay: 3000 }}
+      >
+        {itemData.map((item, idx) => (
+          <SwiperSlide key={idx}>
+            <Image
+              src={item}
+              alt="image"
+              width={400}
+              height={400}
+              className="w-full h-[500px] object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MainSlider;
